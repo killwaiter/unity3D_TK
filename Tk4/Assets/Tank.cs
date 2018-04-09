3d50649683d2cc1eb31ac3e40f8bb6a4174003d6
killwaiter <1765587173@qq.com>
2018-04-09 23:56:25 +0800

    init unity3d_TK commit

---
+++ Tk4/Assets/Tank.cs
@@ -0,0 +1,215 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tank : MonoBehaviour
+{
+    //炮塔炮管轮子履带
+    public Transform turret;
+    public Transform gun;
+    private Transform wheels;
+    private Transform tracks;
+    //炮塔旋转速度
+    private float turretRotSpeed = 0.5f;
+    //炮塔炮管目标角度
+    private float turretRotTarget = 0;
+    private float turretRollTarget = 0;
+    //炮管的旋转范围
+    private float maxRoll = 10f;
+    private float minRoll = -4f;
+
+
+    //轮轴
+    public List<AxleInfo> axleInfos;
+    //马力/最大马力
+    private float motor = 0;
+    public float maxMotorTorque;
+    //制动/最大制动
+    private float brakeTorque = 0;
+    public float maxBrakeTorque = 100;
+    //转向角/最大转向角
+    private float steering = 0;
+    public float maxSteeringAngle;
+
+
+    //马达音源
+    public AudioSource motorAudioSource;
+    //马达音效
+    public AudioClip motorClip;
+
+
+    //玩家控制
+    public void PlayerCtrl()
+    {
+        //马力和转向角
+        motor = maxMotorTorque * Input.GetAxis("Vertical");
+        steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        //制动
+        brakeTorque = 0;
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.leftWheel.rpm > 5 && motor < 0)  //前进时，按下“下”键
+                brakeTorque = maxBrakeTorque;
+            else if (axleInfo.leftWheel.rpm < -5 && motor > 0)  //后退时，按下“上”键
+                brakeTorque = maxBrakeTorque;
+            continue;
+        }
+        //炮塔炮管角度
+        turretRotTarget = Camera.main.transform.eulerAngles.y;
+        turretRollTarget = Camera.main.transform.eulerAngles.x;
+    }
+
+
+
+
+    //开始时执行
+    void Start()
+    {
+        //获取炮塔
+        turret = transform.FindChild("turret");
+        //获取炮管
+        gun = turret.FindChild("gun");
+        //获取轮子
+        wheels = transform.FindChild("wheels");
+        //获取履带
+        tracks = transform.FindChild("tracks");
+        //马达音源
+        motorAudioSource = gameObject.AddComponent<AudioSource>();
+        motorAudioSource.spatialBlend = 1;
+    }
+
+    //每帧执行一次
+    void Update()
+    {
+        //玩家控制操控
+        PlayerCtrl();
+        //遍历车轴
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            //转向
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            //马力
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            //制动
+            if (true)
+            {
+                axleInfo.leftWheel.brakeTorque = brakeTorque;
+                axleInfo.rightWheel.brakeTorque = brakeTorque;
+            }
+            //转动轮子履带
+            if (axleInfos[1] != null && axleInfo == axleInfos[1])
+            {
+                WheelsRotation(axleInfos[1].leftWheel);
+                TrackMove();
+            }
+        }
+
+        //炮塔炮管旋转
+        TurretRotation();
+        TurretRoll();
+        //马达音效
+        MotorSound();
+    }
+
+    //炮塔旋转
+    public void TurretRotation()
+    {
+        if (Camera.main == null)
+            return;
+        if (turret == null)
+            return;
+
+        //归一化角度
+        float angle = turret.eulerAngles.y - turretRotTarget;
+        if (angle < 0) angle += 360;
+
+        if (angle > turretRotSpeed && angle < 180)
+            turret.Rotate(0f, -turretRotSpeed, 0f);
+        else if (angle > 180 && angle < 360 - turretRotSpeed)
+            turret.Rotate(0f, turretRotSpeed, 0f);
+    }
+
+    //炮管旋转
+    public void TurretRoll()
+    {
+        if (Camera.main == null)
+            return;
+        if (turret == null)
+            return;
+        //获取角度
+        Vector3 worldEuler = gun.eulerAngles;
+        Vector3 localEuler = gun.localEulerAngles;
+        //世界坐标系角度计算
+        worldEuler.x = turretRollTarget;
+        gun.eulerAngles = worldEuler;
+        //本地坐标系角度限制
+        Vector3 euler = gun.localEulerAngles;
+        if (euler.x > 180)
+            euler.x -= 360;
+
+        if (euler.x > maxRoll)
+            euler.x = maxRoll;
+        if (euler.x < minRoll)
+            euler.x = minRoll;
+        gun.localEulerAngles = new Vector3(euler.x, localEuler.y, localEuler.z);
+    }
+
+    //轮子旋转
+    public void WheelsRotation(WheelCollider collider)
+    {
+        if (wheels == null)
+            return;
+        //获取旋转信息
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+        //旋转每个轮子
+        foreach (Transform wheel in wheels)
+        {
+            wheel.rotation = rotation;
+        }
+    }
+
+
+    //履带滚动
+    public void TrackMove()
+    {
+        if (tracks == null)
+            return;
+
+        float offset = 0;
+        if (wheels.GetChild(0) != null)
+            offset = wheels.GetChild(0).localEulerAngles.x / 90f;
+
+        foreach (Transform track in tracks)
+        {
+            MeshRenderer mr = track.gameObject.GetComponent<MeshRenderer>();
+            if (mr == null) continue;
+            Material mtl = mr.material;
+            mtl.mainTextureOffset = new Vector2(0, offset);
+        }
+    }
+
+    //马达音效
+    void MotorSound()
+    {
+        if (motor != 0 && !motorAudioSource.isPlaying)
+        {
+            motorAudioSource.loop = true;
+            motorAudioSource.clip = motorClip;
+            motorAudioSource.Play();
+        }
+        else if (motor == 0)
+        {
+            motorAudioSource.Pause();
+        }
+    }
+}
